3c2df4d1a5e85f2d42e53fb8857d0beb1c7de1a8
John Eric Martin <john.eric.martin@gmail.com>
2008-10-18 13:00:48 -0600

    Added skill parser.
    The skill parser uses python as opposed to PHP. A new python module has been added to communicate with the server. This code requires PycURL, demjson (version 1.3 is included in the repo), Beautiful Soup (version 3.0.7a is included in the repo), and a lot of basic python modules.

---
+++ parser/ajax.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# parser/ajax.py
+# Copyright (C) 2008 John Eric Martin <john.eric.martin@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+import StringIO, hashlib, urllib, pycurl, demjson, getpass, sys, re
+
+class ajax:
+	__login_ok = False
+	__backend_url = "https://gigadelic.homelinux.net:55517/backend.php"
+	__email = ""
+	__password = ""
+	__salt = ""
+	__connection = pycurl.Curl()
+
+	def __process_headers(self, data):
+		headerMatcher = re.compile(r"^([^:]+): (.*)$")
+		headers = { }
+
+		for i in data.split(u"\r\n"):
+			match = headerMatcher.match(i)
+			if match:
+				headers[match.group(1).strip()] = match.group(2).strip()
+
+		return headers
+
+	def __get_salt(self):
+		json = self.request({u"action":u"salt"})[0]
+		if json.has_key(u"error"):
+			print u"Error fetching authentication salt:\n%s" % json[u"error"]
+			return
+
+		self.__salt = json[u"salt"].encode("ascii")
+
+	def request(self, action):
+		response_content = StringIO.StringIO()
+		response_headers = StringIO.StringIO()
+
+		# Check the action argument is valid (to some extent)
+		if type(action) == dict:
+			post = { "request" : demjson.encode({u"actions":[action]},
+				escape_unicode=False, encoding="ascii") }
+		elif type(action) == list:
+			post = { "request" : demjson.encode({u"actions":action},
+				escape_unicode=False, encoding="ascii") }
+		else:
+			return [{u"error":u"The argument is not an object or array."}]
+
+		# Handle the authentication
+		if self.__salt and self.__email and self.__password:
+			post["email"] = self.__email
+
+			pass_hash = hashlib.sha1()
+			pass_hash.update(self.__salt)
+			pass_hash.update(self.__password)
+			pass_hash = pass_hash.hexdigest()
+
+			request_hash = hashlib.sha1()
+			request_hash.update(pass_hash)
+			request_hash.update(post["request"])
+			request_hash = request_hash.hexdigest()
+
+			post["pass"] = request_hash
+
+		post = urllib.urlencode(post)
+
+		# Make the request
+		self.__connection.setopt(pycurl.URL, self.__backend_url)
+		self.__connection.setopt(pycurl.HEADERFUNCTION, response_headers.write)
+		self.__connection.setopt(pycurl.WRITEFUNCTION, response_content.write)
+		self.__connection.setopt(pycurl.CAINFO, "../client/res/ca.crt")
+		self.__connection.setopt(pycurl.SSLVERSION, pycurl.SSLVERSION_SSLv3)
+		self.__connection.setopt(pycurl.POSTFIELDS, post)
+		self.__connection.setopt(pycurl.POST, 1)
+		self.__connection.perform()
+
+		# Get the response headers
+		headers = unicode(response_headers.getvalue(), "utf-8")
+		headers = self.__process_headers(headers)
+		if headers.has_key(u"X-JSON") == False:
+			return [{u"error":u"X-JSON header missing."}]
+
+		response_code = self.__connection.getinfo(pycurl.HTTP_CODE)
+		if response_code != 200:
+			return [{u"error":u"Server returned HTTP response code %d"
+				u"but 200 expected." % response_code}]
+
+		response_content = unicode(response_content.getvalue(), "utf-8")
+		if response_content:
+			return [{u"error":u"Unexpected content found in message "
+				u"body:\n%s" % response_content}]
+
+		# Decode the JSON
+		json_match = re.compile(r"^\((.+)\)$").match(headers[u"X-JSON"])
+		if json_match == False:
+			return [{u"error":u"Error parsing X-JSON header."}]
+
+		# Check that the JSON decoded right
+		try:
+			json = demjson.decode( json_match.group(1) )
+		except demjson.JSONError, e:
+			return [{u"error":u"JSON decode error: %s" % e.pretty_description()}]
+
+		# Make sure the response makes sense
+		if type(json) != list:
+			return [{u"error":u"Server response should be an array."}]
+		if len(json) < 1:
+			return [{u"error":u"Server response should have at least 1 item."}]
+
+		return json
+
+	def login(self, backend_url = False, email = False, password = False):
+		if self.__login_ok:
+			return
+
+		print u"Please enter the following info (leaving an entry blank will " \
+			u"use the value shown in brackets)"
+		print u""
+
+		default_url = backend_url if backend_url else self.__backend_url
+		default_password = password if password else self.__password
+		default_email = email if email else self.__email
+
+		# Backend URL
+		sys.stdout.write(u"Backend URL [%s]: " % default_url)
+		temp = sys.stdin.readline().strip()
+		self.__backend_url = temp if temp else default_url
+
+		# Get the password salt
+		self.__get_salt()
+
+		# Email
+		sys.stdout.write(u"Email [%s]: " % default_email)
+		temp = sys.stdin.readline().strip()
+		self.__email = temp if temp else default_email
+
+		# Password
+		temp = getpass.getpass(u"Password: ").strip()
+		self.__password = temp if temp else default_password
+
+		self.__login_ok = True
+
+	def request_id(self, table, column, value):
+		action = { u"action":u"select", u"table":table, u"columns":[u"id"],
+			u"where":[{u"column":column, u"type":u"equals", u"value":value}],
+			u"where_type":u"and", u"limit":1 }
+
+		result = self.request(action);
+		result = result[0];
+
+		if result.has_key(u"error"):
+			return -1
+
+		rows = result[u"rows"]
+		if len(rows) != 1:
+			return -1
+
+		return rows[0][u"id"]
+
+	def update_relation(self, table, id_value, name_ja, name_en):
+		action = { u"action":u"update", u"table":table, u"columns":[u"name_ja",
+			u"name_en"], u"where":[{u"column":"id", u"type":u"equals",
+			u"value":id_value}], u"where_type":"and",
+			u"rows":[{u"name_ja":name_ja, u"name_en":name_en}] }
+
+		return self.request(action);
+
+	def insert_relation(self, table, name_ja, name_en):
+		action = {u"action":u"insert", u"table":table,u"columns":[u"name_ja",
+			u"name_en"], u"rows":[{u"name_ja":name_ja, u"name_en":name_en}] }
+
+		return self.request(action);
+
+	def handle_relation(self, table, name_ja, name_en):
+		id_value = self.request_id(table, u"name_ja", name_ja);
+		if id_value == -1:
+			# TODO: Error check this
+			resp = self.insert_relation(table, name_ja, name_en)
+			return resp[0][u"ids"][0];
+		else:
+			# TODO: Error check this
+			self.update_relation(table, id_value, name_ja, name_en)
+			return id_value
---
+++ parser/parse_skills.py
@@ -0,0 +1,255 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# parser/parse_skills.py
+# Copyright (C) 2008 John Eric Martin <john.eric.martin@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+import BeautifulSoup, demjson, random, time
+import ajax, urllib, urllib2, codecs, sys, re, os
+
+def strip_tags(value):
+	return re.sub(r"<[^>]*?>", u"", value)
+
+def cache_file(url, post, filename):
+	if not os.path.exists(filename):
+		print u"Downloading file '%s'..." % filename
+
+		# Open the url
+		page = urllib2.urlopen(url, urllib.urlencode(post))
+
+		# Save the url to the file
+		handle = open(filename, "w")
+		handle.write( page.read() )
+		handle.close()
+
+		# Sleep for 1-5 seconds
+		time.sleep(random.uniform(1000000, 5000000) / 1000000)
+
+	# Open the file
+	handle = codecs.open(filename, encoding="euc-jp")
+	page = handle.read()
+	handle.close()
+
+	return page
+
+def parse_skill(page, ajax_inst, done = 1, total = 1):
+	soup = BeautifulSoup.BeautifulSoup(page, fromEncoding="utf-8")
+
+	info = soup.findAll(name=u"td",
+		attrs={u"class":u"viewS01", u"bgcolor":u"F0FFF0"})
+	if len(info) != 2:
+		print u"Error parsing skill."
+		sys.exit()
+
+	name = info[0].findAll(name=u"b", limit=1)
+	if len(name) < 1:
+		print u"Error parsing skill name."
+		sys.exit()
+
+	name = name[0].extract().renderContents(encoding=None).strip()
+
+	elements = info[0].findAll(name=u"font", limit=1)
+	if len(elements) < 1:
+		print u"Error parsing skill."
+		sys.exit()
+
+	elements = elements[0].renderContents(encoding=None)
+	elements = strip_tags( elements.replace(u"　", u" ").strip() )
+	elements = elements.replace(u"相性:", u"相性 ")
+	elements = elements.replace(u"コスト:", u"コスト ")
+	elements = elements.replace(u"威力:", u"威力 ")
+	elements = elements.replace(u"依存:", u"依存 ")
+	elements = elements.replace(u"遺伝:", u"遺伝 ")
+	elements = elements.strip().split(u" ")
+
+	if len(elements) != 12:
+		print u"Error parsing skill."
+		sys.exit()
+
+	desc = info[1].findAll(name=u"font", limit=1)
+	if len(desc) < 1:
+		print u"Error parsing skill description."
+		sys.exit()
+
+	desc = desc[0].renderContents(encoding=None).replace(u"　", u" ").strip()
+
+	row = { u"hp_cost":0, u"mp_cost":0, u"mag_cost":0, u"icon":u"SB01_0001a",
+		u"name_en":u"Untitled", u"class":-1, u"rank":-1, "player_only":0 }
+
+	# Parse the skill cost
+	for cost in elements[5].split(u"/"):
+		match = re.compile(r"^HP([0-9]+)\%$").match(cost)
+		if match:
+			row[u"hp_cost"] = match.group(1)
+			continue
+
+		match = re.compile(r"^MP([0-9]+)$").match(cost)
+		if match:
+			row[u"mp_cost"] = match.group(1)
+			continue
+
+		match = re.compile(r"^MAG([0-9]+)$").match(cost)
+		if match:
+			row[u"mag_cost"] = match.group(1)
+
+	row[u"name_ja"] = name
+	row[u"desc_ja"] = desc
+	row[u"desc_en"] = u"依存: %s" % elements[9]
+	row[u"expert"] = ajax_inst.handle_relation(u"db_expert",
+		u"不詳", u"Unknown");
+	#row[u"expert"] = ajax_inst.handle_relation(u"db_expert",
+	#	elements[9], u"Untitled");
+	row[u"category"] = ajax_inst.handle_relation(u"db_category",
+		elements[0], u"Untitled");
+	row[u"action_type"] = ajax_inst.handle_relation(u"db_action_type",
+		elements[1], u"Untitled");
+	row[u"affinity"] = ajax_inst.handle_relation(u"db_affinity",
+		elements[3], u"Untitled");
+
+	row[u"power"] = -1;
+	try:
+		row[u"power"] = int(elements[7]);
+	except:
+		pass
+
+	inherit = { }
+	inherit[u"？"] = -2 # unknown
+	inherit[u"遺伝不可"] = -1 # none
+	inherit[u"ブレス"] = 0 # breath
+	inherit[u"羽"] = 1 # wing
+	inherit[u"突撃"] = 2 # pierce
+	inherit[u"牙"] = 3 # fang
+	inherit[u"爪"] = 4 # claw
+	inherit[u"針"] = 5 # needle
+	inherit[u"武器"] = 6 # sword
+	inherit[u"乙女"] = 7 # strange (daughter?)
+	inherit[u"目"] = 8 # special
+	inherit[u"無制限"] = 99 # any
+
+	if inherit.has_key(elements[11]):
+		row[u"inheritance"] = inherit[ elements[11] ]
+	else:
+		print u"Unknown inheritance type '%s'." % elements[11]
+		sys.exit()
+
+	action = { u"action":u"insert", u"table":u"db_skills",
+		u"columns":row.keys(), u"user_data":u"parse_skills_insert",
+		u"rows":[row] }
+
+	result = ajax_inst.request(action)[0]
+	if result.has_key(u"error"):
+		print u"Error adding skill to the database:\n%s" % result[u"error"]
+		print u"Original JSON request sent was:\n%s" % demjson.encode(action)
+		sys.exit()
+	else:
+		print u"Skill '%s' added to the database at row %d (%d of %d)" \
+			% ( name, result[u"ids"][0], done, total )
+
+# ---------
+# Main Code
+# ---------
+
+random.seed()
+if not os.path.exists("cache"):
+	os.mkdir("cache")
+
+ajax_inst = ajax.ajax()
+ajax_inst.login()
+
+print u""
+print u"Finding the skill list..."
+
+url = "http://artifact.sakura.ne.jp/imagine/";
+
+keys = ["chkbox12", "chkbox2", "chkbox31", "chkbox32", "chkbox33",
+	"chkbox34", "chkbox35", "chkbox36", "chkbox37", "chkbox38", "chkbox39",
+	"data1_1_old", "data1_2_old", "data1_3_old", "data1_4_old", "data2_1",
+	"data2_1_old", "data2_2_old", "data2_3_old", "data2_4_old", "data3_1_old",
+	"data3_2_old", "data3_3_old", "data3_4_old", "data4_1_old", "data4_2_old",
+	"data4_3_old", "data4_4_old", "data5_1_old", "data5_2_old", "data5_3_old",
+	"data5_4_old", "data6_1_old", "data6_2_old", "data6_3_old", "data6_4_old",
+	"data7_1_old", "data7_2_old", "data7_3_old", "data7_4_old", "data7_5_old",
+	"data7_6_old", "devil_lv", "hist", "iden_flag", "old_view", "slct1",
+	"slct1Label", "state", "state_old", "tree_list"];
+
+post = { };
+for key in keys:
+	post[key] = "";
+
+post["state"] = 1;
+
+page = cache_file(url, post, "cache/index.html")
+soup = BeautifulSoup.BeautifulSoup(page)
+
+# Get the select option for showing the skill list of all skills sorted by name
+selects = soup.findAll(name=u"select", attrs={u"name":u"data2_1"})
+if len(selects) != 1:
+	print u"Error parsing skill list."
+	sys.exit()
+
+data2_1 = [ ]
+for tag in selects[0].findAll(name=u"option"):
+	data2_1.append( tag.renderContents(encoding=None) )
+
+if len(data2_1) < 1:
+	print u"Error parsing skill list."
+	sys.exit()
+
+post["data2_1"] = data2_1[0].encode("euc-jp")
+post["state_old"] = post["state"]
+
+print u"Reading the skill list..."
+
+page = cache_file(url, post, "cache/list.html")
+soup = BeautifulSoup.BeautifulSoup(page)
+
+# Get the skill ids (data2_2 values) from the list of skills
+selects = soup.findAll(name=u"select", attrs={u"name":u"data2_2"})
+if len(selects) != 1:
+	print u"Error parsing skill ids."
+	sys.exit()
+
+data2_2 = [ ]
+for tag in selects[0].findAll(name=u"option", attrs={u"value":True}):
+	value = tag["value"].strip()
+	if value:
+		data2_2.append(value)
+
+post["data2_1_old"] = post["data2_1"];
+
+print u"Parsing the skills..."
+
+done = 0
+total = len(data2_2)
+
+# Loop over all the codes and parse the skills
+for code in data2_2:
+	done += 1
+
+	# Set the filename
+	filename = u"cache/%s.html" % code
+
+	# Set the POST data
+	post["data2_2"] = code.encode("euc-jp");
+
+	# Get the file
+	page = cache_file(url, post, filename)
+
+	# Parse the skill
+	parse_skill(page, ajax_inst, done, total)
+
+print u"Parsing done! Enjoy your skills!!!"
